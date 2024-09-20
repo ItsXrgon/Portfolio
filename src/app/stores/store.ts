0e5d6403c5d513488052d45219fc296a541d39cc
itsXrgon <ali.koheil@gmail.com>
2024-09-21 00:08:05 +0300

    fix: comment persistor resetter

--- src/app/stores/store.ts
+++ src/app/stores/store.ts
@@ -17,7 +17,7 @@ export const store = configureStore({
 	},
 });
 export const persistor = persistStore(store);
-persistor.purge();
+// persistor.purge();
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
