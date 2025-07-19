d56c92abee4da89a9cfc82114f64e1e5ed4fea50
itsXrgon <ali.koheil@gmail.com>
2025-07-19 20:19:00 +0300

    feat: remove nexjs slug routing

--- src/app/[[...slug]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Desktop from "@/app/_components/Desktop/Desktop";
-
-export function generateStaticParams() {
-	return [{ slug: [""] }];
-}
-
-export default function Page() {
-	return <Desktop />;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Metadata } from "next";
 import { Cairo } from "next/font/google";
+import Head from "next/head";
 import React from "react";
 
 import Taskbar from "@/app/_components/Taskbar/Taskbar";
@@ -9,6 +10,7 @@ import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import ReduxProvider from "@/providers/ReduxProvider";
 
 import "../styles/globals.css";
+import Desktop from "./_components/Desktop/Desktop";
 
 const cairo = Cairo({
 	subsets: ["latin", "arabic", "latin-ext"],
@@ -21,13 +23,48 @@ export const metadata: Metadata = {
 	applicationName: "itsXrgon",
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+export default function RootLayout() {
 	return (
 		<html lang="en" className={cairo.className}>
+			<Head>
+				<script
+					src="https://tinylytics.app/embed/bXzCJ2LkasASM6jD1SBg.js?kudos&hits&webring&countries&uptime"
+					defer
+				/>
+				{/* favicon */}
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="icon" href="/favicon.ico" />
+				{/* Android Chrome */}
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="192x192"
+					href="/android-chrome-192x192.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="512x512"
+					href="/android-chrome-512x512.png"
+				/>
+			</Head>
 			<body>
 				<div id="root">
 					<Analytics />
@@ -37,10 +74,10 @@ export default function RootLayout({
 							<ReduxProvider>
 								<main
 									style={{
-										height: "calc(100vh - 3.5rem)",
+										height: "calc(100vh - 4rem)",
 									}}
 								>
-									{children}
+									<Desktop />
 								</main>
 								<Taskbar />
 							</ReduxProvider>
